2bfbf4fe009cc75a502818f7355b2b7cd105ea09
ploynoii <kororo50@hotmail.com>
2018-02-20 12:01:37 +0700

    Add Fragment Success

--- app/src/main/java/ploypattara/snru/ac/th/emercall/MainActivity.java
+++ app/src/main/java/ploypattara/snru/ac/th/emercall/MainActivity.java
@@ -3,11 +3,22 @@ package ploypattara.snru.ac.th.emercall;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import ploypattara.snru.ac.th.emercall.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+
+
+       // Add Fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contenMainFragment, new MainFragment()).commit();
+        }
+
+    } //Main Method
+} //Main Class
