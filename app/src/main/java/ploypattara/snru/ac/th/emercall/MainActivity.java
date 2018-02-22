e795297106a15c7af5c443b90403a512a1dcbc90
ploynoii <kororo50@hotmail.com>
2018-02-22 16:39:47 +0700

    Fin 22/02/61

--- app/src/main/java/ploypattara/snru/ac/th/emercall/MainActivity.java
+++ app/src/main/java/ploypattara/snru/ac/th/emercall/MainActivity.java
@@ -8,7 +8,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
+import ploypattara.snru.ac.th.emercall.fragment.HospitalFragment;
 import ploypattara.snru.ac.th.emercall.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +25,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Police Controller
+        TextView policsTextView = findViewById(R.id.txtPolice);
+        policsTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contenMainFragment, new MainFragment()).commit();
+                drawerLayout.closeDrawers();
+            }
+        });
+
+
+//        Hospital Controller
+        TextView hospitalTextView = findViewById(R.id.txtHospital);
+        hospitalTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contenMainFragment, new HospitalFragment()).commit();
+                drawerLayout.closeDrawers();
+            }
+        });
+
+
+//        Insurance Controller
+        TextView insuranceTextView = findViewById(R.id.txtInsurance);
+        insuranceTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contenMainFragment, new MainFragment()).commit();
+                drawerLayout.closeDrawers();
+            }
+        });
+
+
+
+//        Exit Controller
+        TextView exitTextView = findViewById(R.id.txtExit);
+        exitTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contenMainFragment, new MainFragment()).commit();
+                drawerLayout.closeDrawers();
+            }
+        });
+
+
+
 
 //        Create Toolbar
         Toolbar toolbar = findViewById(R.id.toolbarMain);
